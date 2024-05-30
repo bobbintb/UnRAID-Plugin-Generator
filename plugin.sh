333125c79109fbdcc7ea301b176de96387d26310
bobbintb <bobbintb@gmail.com>
2024-05-30 16:45:52 -0600

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -22,11 +22,11 @@ while IFS= read -r line; do
 done < $CONFIG_FILE
 
 for key in "${keys[@]}"; do
-  new_key=$(printf '%-${target_key_length}s' '"$key"')
+  new_key=$(printf "%-${target_key_length}s" "$key")
   echo "$new_key"
   #new_value="${config[$key]}"
-  PLUGIN="${PLUGIN}
-<!ENTITY ${new_key}${!key}>"
+  PLUGIN='${PLUGIN}
+<!ENTITY ${new_key}"${!key}">'
   done
   PLUGIN="${PLUGIN}
 ]>"
