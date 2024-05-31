0469ec738631f8452ceb068f4c154780f615eb1e
bobbintb <bobbintb@gmail.com>
2024-05-31 14:36:39 -0600

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
+show_help() {
+  echo "Usage: $0 [options]"
+  echo
+  echo "Options:"
+  echo "  -a, --author    The author of the plugin."
+  echo "  -c, --config    Specify config file location. (default: ./plugin.cfg)"
+  echo "  -i, --input	  The directory of the plugin's source code. (default: ./src/)"
+  echo "  -m, --md5       The MD5 hash of the plugin *.txz file. (required but automatically provided)"
+  echo "  -n, --name      The name of the plugin. (required)"
+  echo "  -r, --repo      The name of the git repo for the plugin."
+  echo "  -u, --url       The URL of the *.plg file. (required for automatic checking and updating of the plugin)"
+  echo "  -v, --version   The version of the plugin. If not specified it will be generated in the standard Limetech format (e.g 2023.07.21a)"
+  echo
+  echo "  -l, --min       The minimun version of UnRAID allowed for the plugin. Typically used to prevent compatibility issues."
+  echo "  -x, --max       The maximum version of UnRAID allowed for the plugin. Typically used to prevent compatibility issues."
+  echo "  -h, --help      Display this help message."
+
+  exit 0
+}
+config="./plugin.cfg"
+OPTIONS=$(getopt -o a:c:i:m:n:r:u:v:l:x:h --long author:,config:,input:,md5:,name:,repo:,url:,version:,min:,max:,help -- "$@")
+eval set -- "$OPTIONS"
+
 # Load the config file
-CONFIG_FILE="./plugin.cfg"
-if [[ -f "$CONFIG_FILE" ]]; then
-  . $CONFIG_FILE
+if [[ -f "$config" ]]; then
+  . $config
 else
-  echo "Config file not found: $CONFIG_FILE"
+  echo "Config file not found: $config"
   exit 1
 fi
 
 OUTPUT_FILE="${name}.plg"
 
 create_entity() {
-longest_key_length=$(awk -F'=' '/=/{l=length($1); if(l>max) max=l} END {print max}' "$CONFIG_FILE")
+longest_key_length=$(awk -F'=' '/=/{l=length($1); if(l>max) max=l} END {print max}' "$config")
 target_key_length=$((longest_key_length + 3))
 
 while IFS= read -r line; do
     if [[ $line == *"="* ]]; then
         keys+=("${line%%=*}")
     fi
-done < $CONFIG_FILE
+done < $config
 
 for key in "${keys[@]}"; do
   new_key=$(printf "%-${target_key_length}s" "$key")
