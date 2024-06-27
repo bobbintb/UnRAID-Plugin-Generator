5b712b7fe7814df77d1a06c30c34d7410790a15c
bobbintb <bobbintb@gmail.com>
2024-06-27 13:41:54 -0600

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -79,7 +79,7 @@ getver(){
   version=$(date +"%Y.%m.%d")
   datepattern='ENTITY version\s+"([^"]+)"'
   echo "Current date: ${version}"
-  url=$(echo "$pluginURL" | sed 's/;\|&//g')
+  url=$(echo "$pluginURL" | sed 's/&/\$/g; s/;//g')
   eval "url=\"$url\""
   echo "Downloading previous version: ${url}"
   wget $url
